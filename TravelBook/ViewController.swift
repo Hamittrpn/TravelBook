6c3f6367b9caa0b44ab25c2edaefbcb8ffe1da34
Hamit Tırpan <hamittirpan@gmail.com>
2019-09-30 23:38:15 +0300

    added ListViewController

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -16,15 +16,16 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     @IBOutlet weak var commentText: UITextField!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var mapView: MKMapView!
-    var locationManager = CLLocationManager() // Konum alma vb. işlemler için kullanırım.
     
+    var locationManager = CLLocationManager() // Konum alma vb. işlemler için kullanırım.
     var chosenLatitude = Double()
     var chosenLongitude = Double()
     
+    var selectedTitle = ""
+    var selectedTitleId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saved))
         
         mapView.delegate = self
         locationManager.delegate = self
@@ -38,6 +39,10 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         
         mapView.addGestureRecognizer(gestureRecognizer)
     }
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saved))
+    }
+    
     @objc func saved(){
     
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -105,5 +110,6 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
     }
+    
 }
 
