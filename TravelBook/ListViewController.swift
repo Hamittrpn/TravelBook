8b1b1e2d9084184441073a6dc8134f5c6b66d6bf
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-04 17:40:21 +0300

    Added navigation and annotation, delete func,

--- TravelBook/ListViewController.swift
+++ TravelBook/ListViewController.swift
@@ -27,7 +27,11 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         getData()
     }
     
-    func getData(){
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newPlace"), object: nil)
+    }
+    
+    @objc func getData(){
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
@@ -91,6 +95,43 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
             let destinationVC = segue.destination as! ViewController
             destinationVC.selectedTitle = chosenTitle
             destinationVC.selectedTitleId = chosenTitleId
+        }
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = idArray[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0{
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID{
+                            
+                            if id == idArray[indexPath.row]{
+                                context.delete(result)
+                                titleArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do{
+                                    try context.save()
+                                }catch{
+                                    print("Error")
+                                }
+                            }
+                        }
+                    }
+                }
+            }catch{
+                print("Error")
+            }
         }
     }
 }
--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -24,6 +24,11 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     var selectedTitle = ""
     var selectedTitleId : UUID?
     
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotatitonLatitude = Double()
+    var annotationLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,13 +37,134 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         // Kullanıcının konumunu ne kadarlık bir keskinlikte olacağına karar veriyorum.Burada bir çok seçenek var.Eğer,Best ile çalışırsam en mükemmel keskinliği alır. Fakat Telefon pilini fazla harcar.
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization() // Kullanıcıdan location için izin istiyorum.Uygulamamı kullanırken !
-        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingLocation() // Kullanıcı lokasyonunu alıyorum.
+        
+
         
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation))
         gestureRecognizer.minimumPressDuration = 3 // Kaç saniye basılı tutunca algılanacak
-        
         mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if selectedTitle != "" {
+            //Core Data İşlemleri
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = selectedTitleId!.uuidString
+            // Sadece id'si bu olan veriyi getir
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+               let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as!  [NSManagedObject]{
+                        if let title = result.value(forKey: "title") as? String{
+                            annotationTitle = title
+                            
+                            if let subtitle = result.value(forKey: "subtitle") as? String{
+                                annotationSubtitle = subtitle
+                                
+                                if let latitude = result.value(forKey: "latitude") as? Double{
+                                    annotatitonLatitude = latitude
+                                    
+                                    if let longitude = result.value(forKey: "longitude") as? Double{
+                                        annotationLongitude = longitude
+                                        
+                                        let annotation = MKPointAnnotation()
+                                        annotation.title = annotationTitle
+                                        annotation.subtitle = annotationSubtitle
+                                        let coordinate = CLLocationCoordinate2DMake(annotatitonLatitude, annotationLongitude)
+                                        annotation.coordinate = coordinate
+                                        
+                                        mapView.addAnnotation(annotation)
+                                        nameText.text = annotationTitle
+                                        commentText.text = annotationSubtitle
+                                        
+                                        // Aldığım location bana yeter artık almayı durdur.
+                                        locationManager.stopUpdatingLocation()
+                                        
+                                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                        let region = MKCoordinateRegion(center: coordinate, span: span)
+                                        mapView.setRegion(region, animated: true)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }catch{
+                let alertController = UIAlertController(title: "Attention", message: "Something went wrong", preferredStyle: UIAlertController.Style.alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if selectedTitle == "" {
+            // Kullanıcının kendi konumunu alıyorum.
+            let location = CLLocationCoordinate2D.init(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+            // Zoom yapmak için kullanıyorum.Oranlar ne kadar düşük ise zoom oranı o kadar yüksek.
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            // Region oluşturuyorum.Bu region belirlediğim konumu ve zoom in oranını alacak
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+        }else{
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // Navigation açmak için kullanıcının kendi konumundan işaretlediği konuma gideceğim için kullanıcının konumunda pin göstermek istemiyorum.Bu onun ayarı.
+        if annotation is MKUserLocation{
+            return nil
+        }
+        
+        let reuseId = "myAnnotation"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil{
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true // Ekstra bir baloncuk ile bilgi gösterimi
+            pinView?.tintColor = UIColor.blue // Annotationlar kırmızı çıkıyordu.Rengi buradan değiştirebilirim.
+            
+            let button = UIButton(type: UIButton.ButtonType.detailDisclosure) // Detay göstermek için buton
+            pinView?.rightCalloutAccessoryView = button // Pin'e tıkladığımda bilgi kartının sağ tarafına İnfo logosu şeklinde buton
+            
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    // rightCalloutAccessoryView'a (İnfo Logosu) neler olacağını kontrol edeceğim.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if selectedTitle != ""{
+            
+            // Koordinatlar ve yerler arasında ilişki kurar.
+            let requestLocation = CLLocation(latitude: annotatitonLatitude, longitude: annotationLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { (placemarks, error) in
+                // Bu yapıya Closure deniyor. Bir işlem sonucu bana ya bir error ya da bir dizi(sonuç) dönüyor.
+                if let placemark = placemarks{
+                    if placemark.count > 0 {
+                        
+                        //MapItem oluşturabilmek için MKPlacemark'tan obje oluşturmam gerekiyor.Bu objeyide Yukarıda Geocoder metodundan alıyoruz.
+                        let newPlacemark = MKPlacemark(placemark: placemark[0])
+                        let item = MKMapItem(placemark: newPlacemark) // Navigasyonu açabilmem için MKMapItem gerekli
+                        item.name = self.annotationTitle
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving]
+                        item.openInMaps(launchOptions: launchOptions)
+                    }
+                }
+            }
+        }
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saved))
     }
@@ -69,9 +195,12 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
             
             do{
                 try context.save()
-                let alertController = UIAlertController(title: "SUCCESS", message: "place saved.", preferredStyle: UIAlertController.Style.alert)
-                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                self.present(alertController, animated: true, completion: nil)
+                
+                print("Place Saved")
+                NotificationCenter.default.post(name: NSNotification.Name("newPlace"), object: nil)
+                navigationController?.popViewController(animated: true) // Bir önceki sayfaya dönmek için
+
+                // Kaydetme işleminden sonra
             }catch{
                 let alertController = UIAlertController(title: "ERROR", message: "Something went wrong!", preferredStyle: UIAlertController.Style.alert)
                 alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
@@ -99,16 +228,6 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
             annotation.subtitle = commentText.text
             self.mapView.addAnnotation(annotation)
         }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Kullanıcının kendi konumunu alıyorum.
-        let location = CLLocationCoordinate2D.init(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
-        // Zoom yapmak için kullanıyorum.Oranlar ne kadar düşük ise zoom oranı o kadar yüksek.
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        // Region oluşturuyorum.Bu region belirlediğim konumu ve zoom in oranını alacak
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
     }
     
 }
